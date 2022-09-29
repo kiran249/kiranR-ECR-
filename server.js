a26d55e72d55975143da5d1119ad9850075ee3b9
kiran249 <60708138+kiran249@users.noreply.github.com>
2022-09-29 12:06:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:white;">Java Home App - 10</h1> \n');
+  res.send('<h1 style="color:white;">Kiran R - 11</h1> \n');
 });
 
 app.listen(PORT, HOST);
